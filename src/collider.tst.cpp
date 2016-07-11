cbee9c70e908aafe6d41e81915936e2dcfa14dc3
Cluedrew <cluedrew.of.ink@gmail.com>
2016-07-11 17:48:36 -0400

    Wrote some tests for the collider.

--- src/collider.tst.cpp
+++ src/collider.tst.cpp
@@ -1,6 +1,33 @@
 #include "collider.hpp"
 
-/* Tests for the Collider.
- * I'm going to do it, at least 2 for collides even those it is temp, but
- * I'm out of time.
+/* Tests for the Collider. It may be temperary, but it is also tested.
  */
+
+#include <catch.hpp>
+
+
+
+TEST_CASE("Collider Tests")
+{
+  Collider cA(10, 10, 5);
+  Collider dot(13, 6, 0);
+  Collider cB(0, 10, 10);
+  Collider cC(20, 10, 5);
+
+  SECTION("Overlaps")
+  {
+    CHECK(cA.collides(cB));
+    CHECK(dot.collides(dot));
+  }
+
+  SECTION("Edges")
+  {
+    CHECK(cA.collides(dot));
+    CHECK(cC.collides(cA));
+  }
+
+  SECTION("Seperate")
+  {
+    CHECK_FALSE(cB.collides(cC));
+  }
+}
